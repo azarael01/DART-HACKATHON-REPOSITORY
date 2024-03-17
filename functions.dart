3213c339f861b4854d411245adf716b374e1ca09
azarael01 <139633910+azarael01@users.noreply.github.com>
2024-03-17 21:14:25 +0300

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -1,2 +1,20 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+import 'dart:ffi';
+
+void main() {
+  int num1 = 8;
+  int num2 = 90;
+  
+  int resultAddition = addition(num1, num2);
+  int resultMultiplication = multiplication(num1, num2);
+  
+  print("The result of adding $num1 and $num2 is $resultAddition");
+  print("The result of multiplying $num1 and $num2 is $resultMultiplication");
+}
+
+int addition(int a, int b) {
+  return a + b;
+}
+
+int multiplication(int a, int b) {
+  return a * b;
+}
